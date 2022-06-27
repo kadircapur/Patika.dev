b195a763ae19e9d5502f7d4e5326772ff9a26a5e
Abdulkadir ÇAPUR <86108672+kadircapur@users.noreply.github.com>
2022-06-27 11:22:11 +0300

    Add files via upload

---
+++ GirilenSayilardanMinMaxDegerleriBulma/src/compare_num.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class compare_num {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n;
+        int max = 0, min = 0;
+
+
+        System.out.print("Kac adet sayi gireceksiniz : ");
+        n = input.nextInt();
+        int a = n;
+        int i=1;
+
+
+        while (n > 0) {
+
+            System.out.print(i + "." + "Sayinizi giriniz : ");
+            int num = input.nextInt();
+
+            if (n == a) {
+                max = num;
+                min = num;
+
+            } else {
+                if (num > max) {
+                    max = num;
+                } else if (num < min) {
+                    min = num;
+                }
+
+            }
+            i++;
+            n--;
+        }
+
+        System.out.println("Max : " + max);
+        System.out.println("Min : " + min);
+
+
+    }
+}
