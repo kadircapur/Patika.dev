6c03b73adcc89367d9ec0c6feee2ad90326bf1d9
Abdulkadir ÇAPUR <86108672+kadircapur@users.noreply.github.com>
2022-07-01 22:04:36 +0300

    Add files via upload

---
+++ DizidekiElemanlarinFrekansi/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int A[] = {2, 8, 8, 2, 2, 8, 5, 20};
+
+        int count=0;
+
+        Arrays.sort(A);
+
+        for(int i=0; i<A.length; i++)
+        {
+            count=0;
+            for (int j=0; j<A.length; j++)
+            {
+                if(A[i]==A[j])
+                {
+                    count++;
+                }
+            }
+            if(i==0 && A[i+1]==A[i])
+            {
+                System.out.println(String.format("%d 'den %d tane var.", A[i], count));
+            }
+
+            if(i==0 && A[i+1]!=A[i])
+            {
+                System.out.println(String.format("%d 'den %d tane var.", A[i], count));
+            }
+
+            if(i!=0 && A[i-1]!=A[i])
+            {
+                System.out.println(String.format("%d 'den %d tane var.", A[i], count));
+            }
+
+        }
+    }
+}
